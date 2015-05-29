730c81f599c9a7ba8654145aae3a3081bb506788
sjqlwy <sjqlwy@gmail.com>
2015-05-29 22:09:06 +0800

    首次提交

---
+++ utest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# U盘读写寿命测试:
+# 2015/05/29    pATAq
+# 逗你玩
+echo 即将自动识别并扫描U盘寿命
+sleep 5
+echo 已经自动识别，请稍后...
+sleep 5
+echo 您的U盘读写寿命剩余
+sleep 2
+echo 1976/438
+sleep 2
+echo warning: maximal R/W count reached, running e2fsck is recommended
+
